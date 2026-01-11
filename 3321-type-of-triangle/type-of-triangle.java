70b8c92459ef54b875b04459e5066f5690a0bf79
Nithish <146406477+Nithish812@users.noreply.github.com>
2026-01-12 00:30:25 +0530

    Time: 1 ms (48.44%) | Memory: 44.1 MB (11.45%) - LeetSync

--- 3321-type-of-triangle/type-of-triangle.java
+++ 3321-type-of-triangle/type-of-triangle.java
@@ -1,13 +1,14 @@
 class Solution {
     public String triangleType(int[] nums) {
-        Arrays.sort(nums);
-        Set<Integer> set=new HashSet<>();
-        for(int i=0;i<nums.length;i++){
-            set.add(nums[i]);
+          if (nums[0] + nums[1] <= nums[2] || nums[0] + nums[2] <= nums[1] || nums[1] + nums[2] <= nums[0]) {
+            return "none";
         }
-      if (nums[0] + nums[1] <= nums[2]) return "none";
-        if (set.size() == 1) return "equilateral";
-        if (set.size() == 2) return "isosceles";
-        return "scalene";
+        if (nums[0] == nums[1] && nums[1] == nums[2]) {
+            return "equilateral";
+        } else if (nums[0] == nums[1] || nums[1] == nums[2] || nums[2] == nums[0]) {
+            return "isosceles";
+        } else {
+            return "scalene";
+        }
     }
 }
